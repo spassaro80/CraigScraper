7e7f113bbd66abfa5edc23a9ddf70e79b2f60ac0
Stefano Passaro <stefano.passaro@gmail.com>
2020-07-10 11:41:04 +0200

    Code refactoring and cleaning UP

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import index,scraper,bones_scraper
+from .views import index,bones_scraper
 
 urlpatterns = [
     path('', index, name='index'),
-    path('results/', scraper, name='scraper'),
     path('bonesscraper', bones_scraper, name='bones_scraper')
 ]
